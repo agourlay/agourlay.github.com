9cdf5bfae10bd2656c83b94eb765bed0190bed80
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2012-04-24 22:38:32 +0200

    narcissism

--- js/main.js
+++ js/main.js
@@ -4,4 +4,63 @@ $(function() {
     			 'enterOn' : 'timer',
 			 'delayTime' : 240000
 			});
+    initLifestream();
 });
+
+//ugly copy paste from doc example
+function initLifestream(){
+
+	  var count = 0,
+      list = [
+        {
+          service: 'stackoverflow',
+          user: '1097599'
+        },
+        {
+          service: 'twitter',
+          user: 'arnaudgourlay'
+        },
+        {
+          service: 'github',
+          user: 'shagaan'
+        }
+      ];
+
+      Date.prototype.toISO8601 = function(date) {
+          var pad = function (amount, width) {
+              var padding = "";
+              while (padding.length < width - 1 && amount < Math.pow(10, width - padding.length - 1))
+                  padding += "0";
+              return padding + amount.toString();
+          }
+          date = date ? date : new Date();
+          var offset = date.getTimezoneOffset();
+          return pad(date.getFullYear(), 4)
+              + "-" + pad(date.getMonth() + 1, 2)
+              + "-" + pad(date.getDate(), 2)
+              + "T" + pad(date.getHours(), 2)
+              + ":" + pad(date.getMinutes(), 2)
+              + ":" + pad(date.getUTCSeconds(), 2)
+              + (offset > 0 ? "-" : "+")
+              + pad(Math.floor(Math.abs(offset) / 60), 2)
+              + ":" + pad(Math.abs(offset) % 60, 2);
+      };
+
+      $("#lifestream").lifestream({
+        limit: 20,
+        list: list,
+        feedloaded: function(){
+          count++;
+          // Check if all the feeds have been loaded
+          if( count === list.length ){
+            $("#lifestream li").each(function(){
+              var element = $(this),
+                  date = new Date(element.data("time"));
+              element.append(' <abbr class="timeago" title="' + date.toISO8601(date) + '">' + date + "</abbr>");
+            })
+            $("#lifestream .timeago").timeago();
+          }
+        }
+      });
+      
+  }
