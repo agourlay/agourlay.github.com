f6996375c5c5741e0a34754a4621fbf54ca025f7
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2012-11-18 20:33:09 +0100

    redesign in progress

--- js/main.js
+++ js/main.js
@@ -1,63 +1,84 @@
 $(function() {
-    $(".content").hide().fadeIn(1000);
-    $('.content').mouseleave(function(){
-       $('.face').toggleClass('flip');
-      });
-    initLifestream();
+  var service_names = initLifestream();
+  initFilter(service_names);
 });
 
-//ugly copy paste from doc example
-function initLifestream(){
-      var count = 0,
-      list = [
-        {
-          service: 'stackoverflow',
-          user: '1097599'
-        },
-        {
-          service: 'twitter',
-          user: 'arnaudgourlay'
-        },
-        {
-          service: 'github',
-          user: 'agourlay'
-        }
-      ];
+function initLifestream() {
+    var count = 0,
+      list = [{
+        service: 'stackoverflow',
+        user: '1097599'
+      }, {
+        service: 'twitter',
+        user: 'arnaudgourlay'
+      }, {
+        service: 'github',
+        user: 'agourlay'
+      }, {
+        service: 'lastfm',
+        user: 'shagaan'
+      }];
 
-      Date.prototype.toISO8601 = function(date) {
-          var pad = function (amount, width) {
-              var padding = "";
-              while (padding.length < width - 1 && amount < Math.pow(10, width - padding.length - 1))
-                  padding += "0";
-              return padding + amount.toString();
-          }
-          date = date ? date : new Date();
-          var offset = date.getTimezoneOffset();
-          return pad(date.getFullYear(), 4)
-              + "-" + pad(date.getMonth() + 1, 2)
-              + "-" + pad(date.getDate(), 2)
-              + "T" + pad(date.getHours(), 2)
-              + ":" + pad(date.getMinutes(), 2)
-              + ":" + pad(date.getUTCSeconds(), 2)
-              + (offset > 0 ? "-" : "+")
-              + pad(Math.floor(Math.abs(offset) / 60), 2)
-              + ":" + pad(Math.abs(offset) % 60, 2);
-      };
+    var service_names = _.pluck(list, 'service');
 
-      $("#lifestream").lifestream({
-        limit: 14,
-        list: list,
-        feedloaded: function(){
-          count++;
-          // Check if all the feeds have been loaded
-          if( count === list.length ){
-            $("#lifestream li").each(function(){
-              var element = $(this),
-                  date = new Date(element.data("time"));
-              element.append(' <abbr class="timeago" title="' + date.toISO8601(date) + '">' + date + "</abbr>");
-            })
-            $("#lifestream .timeago").timeago();
+    Date.prototype.toISO8601 = function(date) {
+      var pad = function(amount, width) {
+          var padding = "";
+          while(padding.length < width - 1 && amount < Math.pow(10, width - padding.length - 1))
+          padding += "0";
+          return padding + amount.toString();
+        };
+      date = date ? date : new Date();
+      var offset = date.getTimezoneOffset();
+      return pad(date.getFullYear(), 4) + "-" + pad(date.getMonth() + 1, 2) + "-" + pad(date.getDate(), 2) + "T" + pad(date.getHours(), 2) + ":" + pad(date.getMinutes(), 2) + ":" + pad(date.getUTCSeconds(), 2) + (offset > 0 ? "-" : "+") + pad(Math.floor(Math.abs(offset) / 60), 2) + ":" + pad(Math.abs(offset) % 60, 2);
+    };
+
+    $("#lifestream").lifestream({
+      limit: 100,
+      list: list,
+      feedloaded: function() {
+        count++;
+        // Check if all the feeds have been loaded
+        if(count === list.length) {
+          $("#lifestream li").each(function() {
+            var element = $(this),
+              date = new Date(element.data("time"));
+            element.append(' <abbr class="timeago" title="' + date.toISO8601(date) + '">' + date + "</abbr>");
+          });
+          $("#lifestream .timeago").timeago();
+        }
+      }
+    });
+    return service_names;
+}
+
+function initFilter(service_names) {
+
+  $("#filter").live("keyup", function(ev) {
+    var $filter = $(this),
+      keyCode = ev.keyCode || ev.which,
+      search;
+
+    if(keyCode === 27) { //ESC
+      $filter.val("");
+    }
+
+    search = $(this).val();
+    $("#lifestream").find("li").each(function() {
+      var $li = $(this);
+      if(search === "") {
+        $li.show();
+      } else {
+        if(_.contains(service_names, search)) {
+          $li.toggle($li.hasClass("lifestream-" + search));
+        } else {
+          if($li.text().search(search) > 0) {
+            $li.show();
+          } else {
+            $li.hide();
           }
         }
-      }); 
-  }
+      }
+    });
+  });
+}
