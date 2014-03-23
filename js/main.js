d8328b2ab9237c1549a4cd963ec03529d43169f1
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2014-03-23 14:21:24 +0100

    redesign

--- js/main.js
+++ js/main.js
@@ -2,52 +2,20 @@ $(function() {
   $('.needTooltip').tooltip({
     placement : "bottom"
   });
-
-  initLifestream();
+  $('.center-v').center();
+    $(window).bind('resize', function() {
+        $('.center-v').center({transition:300});
+    });
 });
 
-function initLifestream() {
-  var count = 0,
-    list = [{
-      service: 'stackoverflow',
-      user: '1097599'
-    }, {
-      service: 'twitter',
-      user: 'arnaudgourlay'
-    }, {
-      service: 'github',
-      user: 'agourlay'
-    }, {
-      service: 'lastfm',
-      user: 'shagaan'
-    }];
-
-  Date.prototype.toISO8601 = function(date) {
-    var pad = function(amount, width) {
-        var padding = "";
-        while(padding.length < width - 1 && amount < Math.pow(10, width - padding.length - 1))
-        padding += "0";
-        return padding + amount.toString();
-      };
-    date = date ? date : new Date();
-    var offset = date.getTimezoneOffset();
-    return pad(date.getFullYear(), 4) + "-" + pad(date.getMonth() + 1, 2) + "-" + pad(date.getDate(), 2) + "T" + pad(date.getHours(), 2) + ":" + pad(date.getMinutes(), 2) + ":" + pad(date.getUTCSeconds(), 2) + (offset > 0 ? "-" : "+") + pad(Math.floor(Math.abs(offset) / 60), 2) + ":" + pad(Math.abs(offset) % 60, 2);
-  };
-
-  $("#lifestream").lifestream({
-    limit: 20,
-    list: list,
-    feedloaded: function() {
-      count++;
-      // Check if all the feeds have been loaded
-      if(count === list.length) {
-        $("#lifestream li").each(function() {
-          var element = $(this),
-            date = new Date(element.data("time"));
-          element.append(' <abbr class="timeago" title="' + date.toISO8601(date) + '">' + date + "</abbr>");
-        });
-        $("#lifestream .timeago").timeago();
-      }
-    }
-  });
-}
+(function($){
+    $.fn.extend({
+        center: function () {
+            return this.each(function() {
+                var top = ($(window).height() - $(this).outerHeight()) / 2;
+                var left = ($(window).width() - $(this).outerWidth()) / 2;
+                $(this).css({position:'absolute', margin:0, top: (top > 0 ? top : 0)+'px', left: (left > 0 ? left : 0)+'px'});
+            });
+        }
+    }); 
+})(jQuery);
