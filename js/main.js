dc16d4201fafbd1eb83856b089711c0e48be2b4f
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2014-03-23 15:46:01 +0100

    overall improvement

--- js/main.js
+++ js/main.js
@@ -2,20 +2,4 @@ $(function() {
   $('.needTooltip').tooltip({
     placement : "bottom"
   });
-  $('.center-v').center();
-    $(window).bind('resize', function() {
-        $('.center-v').center({transition:300});
-    });
 });
-
-(function($){
-    $.fn.extend({
-        center: function () {
-            return this.each(function() {
-                var top = ($(window).height() - $(this).outerHeight()) / 2;
-                var left = ($(window).width() - $(this).outerWidth()) / 2;
-                $(this).css({position:'absolute', margin:0, top: (top > 0 ? top : 0)+'px', left: (left > 0 ? left : 0)+'px'});
-            });
-        }
-    }); 
-})(jQuery);
